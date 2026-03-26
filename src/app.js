7fac3a593103d138a237e357e5dc528421a21337
Magnus T <magnus.tronhus@gmail.com>
2026-03-26 09:29:39 +0100

    Start Repo for group work

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// This tells Express to serve files from your new /node/public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
