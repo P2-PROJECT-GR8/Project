2c7a2ff87074c9dc6127d0cda403670fe429d037
vlr05 <vlr05@hotmail.com>
2026-03-27 12:57:17 +0100

    "post" example added
    pc transfer

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // This tells Express to serve files from your new /node/public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+app.post("/get-username", function (req, res) {
+  console.log("recieved from index.js", req.body);
+  res.json({ username: "admin", status: "recieved", access: "all" });
+});
 
 app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+  console.log("Server running at http://localhost:3000");
 });
--- src/public/index.js
+++ src/public/index.js
@@ -1,6 +1,20 @@
 const btn = document.getElementById("btn");
 const counter = document.getElementById("counter");
 
-btn.addEventListener('click', (e) => {
-    counter.innerText = (Number(counter.innerText) + 1).toString();
-})
+btn.addEventListener("click", (e) => {
+  counter.innerText = (Number(counter.innerText) + 1).toString();
+});
+
+loginHandler();
+//login handler
+function loginHandler() {
+  const respone = fetch("http://localhost:3000/get-username", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ username: "admin" }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
