ee0a1026c20124b8afcd500c6aa501af987f1a31
tr0nhus <magnus.tronhus@gmail.com>
2026-03-27 10:23:10 +0100

    Added decrease button

--- src/public/index.js
+++ src/public/index.js
@@ -1,6 +1,11 @@
 const btn = document.getElementById("btn");
+const btn2 = document.getElementById("btn2");
 const counter = document.getElementById("counter");
 
-btn.addEventListener('click', (e) => {
-    counter.innerText = (Number(counter.innerText) + 1).toString();
-})
+btn.addEventListener("click", (e) => {
+  counter.innerText = (Number(counter.innerText) + 1).toString();
+});
+
+btn2.addEventListener("click", (e) => {
+  counter.innerText = (Number(counter.innerText) - 1).toString();
+});
