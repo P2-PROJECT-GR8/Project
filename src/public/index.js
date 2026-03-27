fc9c62836c7bf459ab59bb356b39765c0aee869a
Dhoof7 <BI27DS@student.aau.dk>
2026-03-27 17:10:52 +0100

    Landing page 2

--- src/public/index.js
+++ src/public/index.js
@@ -1,6 +1,12 @@
-const btn = document.getElementById("btn");
-const counter = document.getElementById("counter");
 
-btn.addEventListener('click', (e) => {
-    counter.innerText = (Number(counter.innerText) + 1).toString();
-})
+document.getElementById("loginForm").addEventListener("submit", function (event){
+const username = document.getElementById("username").value;
+
+if(username.length <= 2){ 
+    event.preventDefault();
+    alert("Username skal være længere end 2 bokstaver");
+
+}
+
+
+}); 
