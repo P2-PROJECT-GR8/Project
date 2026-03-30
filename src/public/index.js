ac5665a07c660da6b9026aec9a974ab463ff6aa8
tr0nhus <magnus.tronhus@gmail.com>
2026-03-30 13:43:04 +0200

    Almost final dashboard

--- src/public/index.js
+++ src/public/index.js
@@ -1,6 +1,14 @@
 const btn = document.getElementById("btn");
 const counter = document.getElementById("counter");
 
-btn.addEventListener('click', (e) => {
-    counter.innerText = (Number(counter.innerText) + 1).toString();
-})
+btn.addEventListener("click", (e) => {
+  counter.innerText = (Number(counter.innerText) + 1).toString();
+});
+
+function showPage(pageId) {
+  document.querySelectorAll(".page").forEach((page) => {
+    page.classList.remove("active");
+  });
+
+  document.getElementById(pageId).classList.add("active");
+}
