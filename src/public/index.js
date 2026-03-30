1187fe638b3a82c68cf22c233c20801a3331bdce
StrikeZ <fpbrauner@gmail.com>
2026-03-30 10:47:42 +0200

    Removed index.js/html
    I removed them because when i would run with express it would only open the index.html and not landing.html

--- src/public/index.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("btn");
-const counter = document.getElementById("counter");
-
-btn.addEventListener("click", (e) => {
-  counter.innerText = (Number(counter.innerText) + 1).toString();
-});
