64a23b9562258595e0586b4173a138d81a260738
StrikeZ <fpbrauner@gmail.com>
2026-03-30 10:46:38 +0200

    Removed the landing page folder

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// This tells Express to serve files from your new /node/public folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "landing.html"));
+});
 
 app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+  console.log("Server running on http://localhost:3000");
 });
