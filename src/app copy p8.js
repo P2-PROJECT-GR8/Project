6bcc6c5fff4adfb2c3bbe6871c0a38e62b679019
Dhoof7 <BI27DS@student.aau.dk>
2026-03-31 20:12:44 +0200

    Create app copy p8.js

---
+++ src/app copy p8.js
@@ -0,0 +1,33 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { validUserName } from "./users.js";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// This tells Express to serve files from your new /node/public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+
+// returns an object with username and their access level
+app.post("/username", function (req, res) {
+  console.log("recieved from index.js", req.body);
+  let userName = req.body.username;
+
+  // returns an object with access level as located from some sort of DB (yet to be implemented)
+  let userObject = validUserName(userName);
+
+  if (userObject.valid) {
+    res.json({ username: userName, status: "200", access: userObject.access });
+  } else {
+    res.json({ status: "404" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
