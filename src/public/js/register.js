50ee7ea5a7191f1fe53455abd1085c4245e0e602
EmilVMB <ebille25@student.aau.dk>
2026-04-16 18:24:00 +0200

    start lav html skift om til div skift

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -28,3 +28,49 @@ const regHandler = async (event) => {
     successMsg.innerText = "";}
   }
 document.getElementById("regBtn").addEventListener("click", regHandler);
+
+const landingSite = document.getElementById("formContainer");
+
+const registerBtn = document.getElementById("regBtn");
+registerBtn.addEventListener("click", (e)=> {
+  e.preventDefault();
+  landingSite.innerHTML = `<label for="regForm"><b>Register here</b></label>
+          <form>
+            <input
+              class="regUser"
+              type="text"
+              id="regInput"
+              placeholder="Enter Username"
+              name="regForm"
+              required
+              minlength="2"
+              size="20"
+            />
+            <button type="submit" class="button" id="regBtn">Register</button>
+            <p id="successMsg" class="success"></p>
+            <p id="ErrorMsg" class="error"></p>
+            <div id="newLogIn" style="display: none;" class="register-link"><a href=""a></div>
+            </form>`;});
+
+const loginSiteBtn= document.getElementById("newLogIn");
+loginSiteBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  landingSite.innerHTML = `<label for="username"><b>Login here</b></label>
+          <form>
+            <input
+              class="username"
+              type="text"
+              id="usernameInput"
+              placeholder="Enter Username"
+              name="username"
+              required
+              minlength="2"
+              size="20"
+            />
+            <button type="submit" class="button" id="LoginBtn">Login</button>
+            <button class="button" id="createSession">Create Session</button>
+            <nav>
+              <a href="" class="register-link">Not in the DB? Register here</a>
+            </nav>
+            <p id="ErrorMsg" class="error"></p>
+            </form>`});
