5883ec7a8057d0df1e085e292a86851f9060bceb
vlr05 <vlr05@hotmail.com>
2026-04-27 18:14:39 +0200

    small changes ++ re added /api/adminfiles

--- src/app.js
+++ src/app.js
@@ -391,6 +391,31 @@ app.get("/api/adminRelations", async (req, res) => {
   }
 });
 
+// return the list of files for the provided userId if user is admin
+app.get("/api/adminFiles", async (req, res) => {
+  console.log("i made it top here yayyyyy");
+  const token = req.cookies.sessionToken;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    // only allow admin to to utilize this endpoint.
+    if (getUser(req).id !== "user:admin") {
+      return res.status(403).send({ messeage: "request denied" });
+    }
+    const targetUser = req.query.userId;
+
+    const userRelations = await accessControl.getUserRelations(targetUser);
+    console.log(userRelations);
+
+    res.json({ files: userRelations });
+  } catch (error) {
+    return res.status(401).send({ message: "Invalid session" });
+  }
+});
+
 app.get("/api/isAdmin", async (req, res) => {
   const token = req.cookies.sessionToken;
   if (!token) {
--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -7,25 +7,75 @@ import { renderHeader } from "./navRenderer.js";
 // wait for DOM load before doing anything
 document.addEventListener("DOMContentLoaded", async () => {
   renderHeader();
-  const userSelect = document.getElementById("user-Datalist");
+  const userSelect = document.getElementById("user-Select");
+  const userDatalist = document.getElementById("user-Datalist");
   const objectSelect = document.getElementById("object-Datalist");
   const display = document.getElementById("main-Display");
-  const userId = "user:jeff";
-  const objectId = "file:1";
+
 
   const usernameres = await fetch("/api/userNames", { credentials: "include" });
   const { userNames } = await usernameres.json();
+  console.log(userNames);
 
-  const pathsres = await fetch(
+  userNames.forEach(user => {
+    if (user === "Admin"){
+      return
+    }
+    let element = document.createElement("option");
+    element.value = user;
+    console.log(element);
+    userDatalist.appendChild(element);
+    
+  });
+
+  // when admin inputs a new name change the  object sleect to contain that users files
+  userSelect.addEventListener("change", async () => {
+      const filelist = await fetch(`/api/adminFiles?userId=user:${userSelect.value}`, {credentials: "include"});
+      if(!filelist.ok){
+        console.error("failed to fetch files");
+      }
+      const { files } = await filelist.json();
+      console.log(files.files);
+      renderFiles(files);
+  });
+  const locatePathsBtn = document.getElementById("submit");
+  locatePathsBtn.addEventListener("click", async () => {
+    const userId = `user:${userSelect.value}`;
+    
+
+    const pathsres = await fetch(
     `/api/adminRelations?userId=${userId}&objectId=${objectId}`,
     { credentials: "include" },
-  );
-  const paths = await pathsres.json();
-  console.log(paths.paths);
+    );
+    const paths = await pathsres.json();
+    console.log(paths.paths);
+
+  });
+
+
+  
+  function renderFiles(files) {
+    objectSelect.innerHTML = "";
+    files.forEach(file => {
+      let element = document.createElement("option");
+      
+      
+    });
+    
+  }
+
+
+
 });
+
+
 
 // render path
 function renderPathToObject(paths) {}
 
 // render admin dashboard
-function adminDash() {}
+function adminDash() {
+
+
+
+}
