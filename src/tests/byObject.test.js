9064ed7b1e2184833527a01d697eadd9a6ffbd64
Dhoof7 <BI27DS@student.aau.dk>
2026-05-05 11:27:06 +0200

    byObject testing

---
+++ src/tests/byObject.test.js
@@ -0,0 +1,35 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { AccessControl } from "../routes/access.js";
+
+//Creating a false database
+function testDb() {
+  return {
+    data: {
+      tupleStore: { byObject: {}, bySubject: {} },
+      schema: {},
+    },
+    read: async () => {},
+    write: async () => {},
+  };
+}
+
+//Creating a test Suite
+describe("addTuple", () => {
+  let db;
+  let ac;
+  // BeforeEach, resets db and ac before every test, so that the test is not affected by what a previous test did to them.
+  beforeEach(() => {
+    db = testDb();
+    ac = new AccessControl(db);
+  });
+
+  it("adds tuple to byObject", async () => {
+    //act
+    await ac.addTuple("user:alice", "owner", "file:1");
+
+    //Assert
+    expect(db.data.tupleStore.byObject["file:1"]).toEqual([
+      { subjectId: "user:alice", relation: "owner" },
+    ]);
+  });
+});
