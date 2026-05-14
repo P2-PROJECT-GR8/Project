b25356405d1ab3ea0b5d80ca6c3770bfb12e6512
Dhoof7 <BI27DS@student.aau.dk>
2026-05-14 09:11:38 +0200

    Update addTupleDuplicates.test.js
    vi.fn tilføjet i addTupleDuplicates

--- src/tests/addTupleDuplicates.test.js
+++ src/tests/addTupleDuplicates.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach, vi } from "vitest";
 import { AccessControl } from "../routes/access.js";
 
 //Creating a false database
@@ -8,8 +8,8 @@ function testDb() {
       tupleStore: { byObject: {}, bySubject: {} },
       schema: {},
     },
-    read: async () => {},
-    write: async () => {},
+    read: vi.fn(),
+    write: vi.fn(),
   };
 }
 
